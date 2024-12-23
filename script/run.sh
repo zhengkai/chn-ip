8676a0cdd377327c4cd5ec1bb8b0ddcc9602a472
Zheng Kai <zhengkai@gmail.com>
2024-12-23 22:06:40 +0800

    up

--- script/run.sh
+++ script/run.sh
@@ -6,22 +6,18 @@ URL="http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest"
 FILE="apnic.txt"
 
 if [ ! -f "$FILE" ]; then
-	echo wget "$URL" -O "$FILE" || exit 1
+	wget "$URL" -O "$FILE" || exit 1
 fi
 
 grep ipv4 "$FILE" | grep CN | awk -F\| '{ printf("%s/%d\n", $4, 32-log($5)/log(2)) }' \
 	> tmp.txt
 
-date +%s >> tmp.txt
-
 cmp tmp.txt ../ipv4.txt && exit 1
 
 cp tmp.txt ../ipv4.txt
 
-TZ="Asia/Shanghai"
-
 TS=$(date +%s)
-DATE=$(date -d @"$TS" '+%Y-%m-%d %H:%M:%S')
+DATE=$(TZ="Asia/Shanghai" date -d @"$TS" '+%Y-%m-%d %H:%M:%S')
 
 (
 	echo "$TS"
