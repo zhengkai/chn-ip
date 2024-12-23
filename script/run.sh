4beff19f0464340b39c58d6d703f056bc44cac88
Zheng Kai <zhengkai@gmail.com>
2024-12-23 21:20:34 +0800

    startup

---
+++ script/run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -x
+
+cd "$(dirname "$0")" || exit 1
+
+URL="http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest"
+FILE="apnic.txt"
+
+wget -q "$URL" -O "$FILE"
+
+grep ipv4 "$FILE" | grep CN | awk -F\| '{ printf("%s/%d\n", $4, 32-log($5)/log(2)) }' \
+	> tmp.txt
+
+cmp tmp.txt ../ipv4.txt && exit 0
+
+cp tmp.txt ../ipv4.txt
+
+TS=$(date +%s)
+DATE=$(TZ="Asia/Shanghai" date -d @"$TS" '+%Y-%m-%d %H:%M:%S')
+
+(
+	echo "$TS"
+	echo "$DATE"
+) > ../time-modified.txt
+
+export DATE
+envsubst < ./tpl.html > ../index.html
+envsubst < ./tpl.md > ../README.md
---
+++ script/tpl.md
@@ -0,0 +1,11 @@
+# 每日更新中国 IPv4 网段
+
+下载地址：<https://zhengkai.github.io/chn-ip/ipv4.txt>
+
+可定期 wget
+
+最后更新时间：${DATE}
+
+APNIC（Asia-Pacific Network Information Centre，亚太互联网络信息中心）是负责亚太地区互联网资源分配和管理的非营利组织，是全球五大区域性互联网注册管理机构（RIR）之一。该组织核心职能之一是向亚太地区的 ISP（互联网服务提供商）、企业、政府机构等分配 IPv4 和 IPv6 地址，本脚本负责每日自动从 `http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest` 抓取最新分配信息
+
+全球版：<https://github.com/zhengkai/ip-range>
