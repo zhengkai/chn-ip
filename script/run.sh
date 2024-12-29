79ac0d8bad9189691be87ecb9b5fbb8f086e7271
Zheng Kai <zhengkai@gmail.com>
2024-12-29 16:50:22 +0800

    up

--- script/run.sh
+++ script/run.sh
@@ -1,17 +1,16 @@
-#!/bin/bash -ex
+#!/bin/bash -x
 
-cd "$(dirname "$0")"
+cd "$(dirname "$0")" || exit 1
 
 URL="http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest"
 FILE="apnic.txt"
 
-ls
-wget "$URL" -O "$FILE"
+wget -q "$URL" -O "$FILE"
 
 grep ipv4 "$FILE" | grep CN | awk -F\| '{ printf("%s/%d\n", $4, 32-log($5)/log(2)) }' \
 	> tmp.txt
 
-cmp tmp.txt ../ipv4.txt && exit 1
+cmp tmp.txt ../ipv4.txt && exit 0
 
 cp tmp.txt ../ipv4.txt
 
